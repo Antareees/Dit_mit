689ee223bb95370725f8b719d1453ecf51e83ab8
Antareees <efimm2004@gmail.com>
2023-04-05 10:30:34 +0400

    add recursive fib; 1,1...

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "Hello, world!";
+int fib(int n)
+{
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+    int n;
+    cout << "n=";
+    cin >> n;
+    cout << fib(n) << " ";
+    cout << endl;
+    return 0;
 }
